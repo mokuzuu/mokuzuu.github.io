5422d60748b15d48e3775d1ebd6b7b00d92129e5
mokuzuu <mokuzuu@users.noreply.github.com>
2021-07-29 12:54:46 +0000

    deploy: 44d5051b98c1692920848168cd3bc1b8b1143f5f

---
+++ _next/static/osK-CXE4Eo_azcXZuA941/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-e74b391d14410ebef55f.js"],"/_error":["static/chunks/pages/_error-8842bdd603648886795b.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
